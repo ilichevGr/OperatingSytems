9d63fbad322754f78f9de0397501d7db1969cd50
ilichevGr <ilichevgrigoriy@gmail.com>
2020-12-28 02:12:11 +0300

    lab7

--- lab_07/main.c
+++ lab_07/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 #define TRUE 1
 #define ARRAY_SIZE 100
@@ -10,10 +11,12 @@ int recordNo = 0;
 int buf[ARRAY_SIZE];
 
 pthread_mutex_t mutex;
+pthread_cond_t pthreadCond;
 
 void* reader(){
     while(TRUE){
         pthread_mutex_lock(&mutex);
+        pthread_cond_wait(&pthreadCond, &mutex);
         printf("TID: %d\nARRAY LENGTH: %d\n",(int)pthread_self(),buf[recordNo]);
         pthread_mutex_unlock(&mutex);
         sleep(1);
@@ -26,6 +29,9 @@ void* writer(){
         recordNo++;
         if (recordNo < ARRAY_SIZE) buf[recordNo] = recordNo;
         printf("RECORD NO: %d\n",recordNo);
+        int rndm = rand() % 100;
+        if (rndm < 50) pthread_cond_broadcast(&pthreadCond);
+        else pthread_cond_signal(&pthreadCond);
         pthread_mutex_unlock(&mutex);
         sleep(1);
     }
@@ -34,6 +40,7 @@ void* writer(){
 int main(void){
     pthread_t threads[threadsCount];
     pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&pthreadCond, NULL);
     pthread_create(&threads[0], NULL, writer, NULL);
     for (int i = 1; i < threadsCount; i++) pthread_create(&threads[i], NULL, reader,NULL);
     for (int i = 0; i < threadsCount; i++) pthread_join(threads[i], NULL);
